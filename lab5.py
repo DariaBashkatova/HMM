8d61b6d4511f8f250b2054113b136607f16a5a27
DariaBashkatova <lp403458@gmail.com>
2017-03-24 21:40:20 +0700

    Правка масштабирования

--- lab5.py
+++ lab5.py
@@ -5,18 +5,16 @@ import matplotlib.pyplot as plt
 
 # масштабирование от 0 до 10
 def Scaling(X, min = 0.0, max = 10.0):
-    i = 0
-    X111 = list(map(lambda x: (max - min) / (np.ndarray.max(x) - np.ndarray.min(x)) * (x - np.ndarray.min(x)) + min, X))
-    # for row in X:
-    #     X[i] = (max - min) / (np.ndarray.max(row) - np.ndarray.min(row)) * (row - np.ndarray.min(row)) + min
-    #     i += 1
+    X = list(map(lambda x: (max - min) / (np.ndarray.max(x) - np.ndarray.min(x)) * (x - np.ndarray.min(x)) + min, X))
+    for i in X:
+        i[i >= 10.0] = 9.0
     return X
 
 
 # квантование
-def Quantization(X, M):
+def Quantization(X, M, max = 10.0):
     # 10 интервалов длиной 10
-    intervals = list(map(lambda x: FloatInterval([x, x + 10 / M]), np.arange(0, 10, 10 / M)))
+    intervals = list(map(lambda x: FloatInterval([x, x + max / M]), np.arange(0, max, max / M)))
     print(intervals)
     # по строкам в dataframe
     for row in X.itertuples():
@@ -26,7 +24,7 @@ def Quantization(X, M):
             for inter in intervals:
                 # если элемент принадлежит интервалу, заменяем этот элемент в dataframe на значение середины этого интервала
                 if row[e] in inter:
-                    X.iloc[row[0], e - 1] = inter.lower
+                    X.iloc[row[0], e - 1] = inter.lower * M / max
                     break
     return X
 
@@ -36,6 +34,7 @@ def alpha_init(o, pi, b, N):
     T = len(o)
     alpha = np.zeros((T, N))
     for i in range(N):
+        y = b[i][o[0]]
         alpha[0][i] = pi[i] * b[i][o[0]]
     return alpha
 
@@ -118,7 +117,7 @@ def estimate_B(o, gamma, ksi, T, K, N, M):
 
 
 def get_estimane(o, N, M, K, T):
-    ln0, lnk, eps = -1, 0, 1e-4
+    ln0, lnk, eps = -1, 0, 1e-2
     A = np.full((N, N), 1 / N)
     B = np.full((N, M), 1 / M)
     pi = np.full(N, 1 / N)
@@ -131,9 +130,8 @@ def get_estimane(o, N, M, K, T):
         for k in range(K):
             alpha = get_alpha(o[k], pi, A, B, N)
             beta = get_beta(o[k], A, B, N)
-
             P = sum(sum(alpha))
-            print(P)
+            # print(P)
             lnk += np.log(P)
             gamma.append(get_gamma(alpha, beta, P, T, N))
             ksi.append(get_ksi(o[k], alpha, beta, P, A, B, N))
@@ -149,29 +147,27 @@ def get_estimane(o, N, M, K, T):
 
 def graf(y, name):
     plt.title(name)
-    x = np.arange(0, 100, 1)
+    x = np.arange(0, len(y[0]), 1)
     plt.plot(x, y[0], marker='o')
     plt.show()
 
 
 # считывание данных
 df_train = pd.read_csv('Training-data.csv')
-df_test = pd.read_csv('Testing-data.csv')
+# df_test = pd.read_csv('Testing-data.csv')
 
 # разбиение выборки на признаки и ответы
 X_train, y_train = df_train.loc[:, 'X1':'X100'], df_train.loc[:, 'class':]
-X_test, y_test = df_test.loc[:, 'X1':'X100'], df_test.loc[:, 'class':]
+# X_test, y_test = df_test.loc[:, 'X1':'X100'], df_test.loc[:, 'class':]
 
 # M - размер алфавита, N - количество скрытых состояний
-M, N = 25, 3
+M, N = 10, 3
 
-# масштабирование и квантование
+# масштабирование и квантование с шагом 10/M
 graf(X_train.values, "Obichnie")
-_X_train = pd.DataFrame(Scaling(X_train.values))
+X_train_scale = Quantization(pd.DataFrame(Scaling(X_train.values)), M)
 
-graf(_X_train.values, "Normirovanie")
-X_train_scale = Quantization(_X_train, M)
-# X_test_scale = Quantization(Scaling(X_test), M)
+graf(X_train.values, "Normirovanie")
 
 # соединение признаков и ответов, чтобы отсортировать выборку по классам (0/1)
 df_train = pd.concat([X_train_scale, y_train], axis=1).sort_values(by='class')
@@ -180,19 +176,22 @@ df_train = pd.concat([X_train_scale, y_train], axis=1).sort_values(by='class')
 X_train_cavity = df_train.loc[df_train['class'] == 0].values[:, 0:100]
 X_train_hill = df_train.loc[df_train['class'] == 1].values[:, 0:100]
 
-X_cavity_4 = np.zeros((len(X_train_cavity), 100 / 4))
-X_hill_4 = np.zeros((len(X_train_hill), 100 / 4))
-i = 0
-for row in X_train_cavity:
-    X_cavity_4[i] = row[::4]
-    i += 1
-print(X_train_cavity)
-print(X_cavity_4)
+X_train_cavity = np.array(list(map(lambda x: x[::3], X_train_cavity)))
+X_train_hill = np.array(list(map(lambda x: x[::3], X_train_hill)))
+
+# T - длина последовательности, K - количество последовательностей
+T, K = len(X_train_cavity[0]), len(X_train_cavity)
 
 graf(X_train_cavity, "cavity")
 graf(X_train_hill, "hill")
 
-# T - длина последовательности, K - количество последовательностей
-T, K = len(X_cavity_4[0]), len(X_cavity_4)
-A_h_cavety, B_h_cavety, pi_h_cavety = get_estimane(X_cavity_4, N, M, K, T)
+A_h_cavety, B_h_cavety, pi_h_cavety = get_estimane(X_train_cavity, N, M, K, T)
 print(A_h_cavety, B_h_cavety, pi_h_cavety)
+for i in range(len(B_h_cavety)):
+    print(sum(B_h_cavety[i]))
+
+# max = 10
+# intervals = list(map(lambda x: FloatInterval([x, x + max / M]), np.arange(0, max, max / M)))
+# for i in range(len(intervals)):
+#     print(intervals[i].lower)
+# print(10.0 in intervals[9])
